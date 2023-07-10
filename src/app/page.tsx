dac498cff513b6fce1775415eca9941343f5b64b
guilherme-liveseo <guilherme@liveseo.com.br>
2023-07-10 11:48:47 -0300

    Iniciando projeto

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
+import Widget from "@/components/Widget";
+
 export default function Home() {
   return (
-    
+    <main className="h-screen bg-zinc-950 flex items-center justify-center">
+      <Widget />
+    </main>
   )
 }
